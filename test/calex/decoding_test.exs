56f42fa17b4e65308f671d16fbdaee920554abc4
Derrick Reimer <derrickreimer@gmail.com>
2021-07-29 16:10:48 -0500

    Add test for Apple structured location

--- test/calex/decoding_test.exs
+++ test/calex/decoding_test.exs
@@ -1,6 +1,8 @@
 defmodule Calex.DecodingTest do
   use ExUnit.Case
 
+  require Logger
+
   test "decodes dates with time zones" do
     data =
       crlf("""
@@ -44,6 +46,50 @@ defmodule Calex.DecodingTest do
                ]
              ]
            ]
+  end
+
+  test "handle Apple structured location field" do
+    data =
+      crlf("""
+      BEGIN:VCALENDAR
+      BEGIN:VEVENT
+      X-APPLE-STRUCTURED-LOCATION;VALUE=URI;X-ADDRESS="500 Nicollet St, Minneapol
+       is, MN, United Stat";X-APPLE-MAPKIT-HANDLE=CAEStwIIrk0QnsWIkObE3qLyARoS CVS
+       PNLitEkpAEUGK8OV0pVrAIpoBCgZDYW5hZGESAkNBGgxTYXNrYXRjaGV3YW4iAlNLKg9EaXZpc2
+       lvbiBOby4gMTEyCVNhc2thdG9vbjoHUzdOIDNQOUIMRm9yZXN0IEdyb3ZlUgpXZWJzdGVyIFN0W
+       gM1MDJiDjUwMiBXZWJzdGVyIFN0igEWVW5pdmVyc2l0eSBIZWlnaHRzIFNEQYoBDEZvcmVzdCBH
+       cm92ZSodRm9yZXN0IEdyb3ZlIENvbW11bml0eSBDaHVyY2gyDjUwMiBXZWJzdGVyIFN0MhRTYXN
+       rYXRvb24gU0sgUzdOIDNQOTIGQ2FuYWRhOC9aJwolCJ7FiJDmxN6i8gESEglUjzS4rRJKQBFBiv
+       DldKVawBiuTZADAQ==;X-APPLE-RADIUS=123.4774275404302;X-APPLE-REFERENCEFRAME=
+       1;X-TITLE=The Wedge:geo:42.145927,-100.585260
+      END:VEVENT
+      END:VCALENDAR
+      """)
+
+    decoded = [
+      vcalendar: [
+        [
+          vevent: [
+            [
+              x_apple_structured_location:
+                {"geo:42.145927,-100.585260",
+                 [
+                   value: "URI",
+                   x_address: "\"500 Nicollet St, Minneapolis, MN, United Stat\"",
+                   x_apple_mapkit_handle:
+                     "CAEStwIIrk0QnsWIkObE3qLyARoS CVSPNLitEkpAEUGK8OV0pVrAIpoBCgZDYW5hZGESAkNBGgxTYXNrYXRjaGV3YW4iAlNLKg9EaXZpc2lvbiBOby4gMTEyCVNhc2thdG9vbjoHUzdOIDNQOUIMRm9yZXN0IEdyb3ZlUgpXZWJzdGVyIFN0WgM1MDJiDjUwMiBXZWJzdGVyIFN0igEWVW5pdmVyc2l0eSBIZWlnaHRzIFNEQYoBDEZvcmVzdCBHcm92ZSodRm9yZXN0IEdyb3ZlIENvbW11bml0eSBDaHVyY2gyDjUwMiBXZWJzdGVyIFN0MhRTYXNrYXRvb24gU0sgUzdOIDNQOTIGQ2FuYWRhOC9aJwolCJ7FiJDmxN6i8gESEglUjzS4rRJKQBFBivDldKVawBiuTZADAQ==",
+                   x_apple_radius: "123.4774275404302",
+                   x_apple_referenceframe: "1",
+                   x_title: "The Wedge"
+                 ]}
+            ]
+          ]
+        ]
+      ]
+    ]
+
+    assert Calex.decode!(data) == decoded
+    assert Calex.encode!(decoded) == data
   end
 
   defp crlf(string) do
